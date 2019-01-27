099a12f8263ddd6d94aca2c5d21c3746f2263d80
Excel Espina <eespina002@citymail.cuny.edu>
2019-01-26 22:11:58 -0500

    Refactored somewhat

--- coop_asmt1/file_rename.sh
+++ coop_asmt1/file_rename.sh
@@ -4,16 +4,17 @@ ORIGINAL_FILES=*.txt
 
 for f in $ORIGINAL_FILES
 do
-    if ["$(wc -l $f)" -eq 0]; then
+    FILE_SIZE="$(wc -l < $f)"
+    if [ $FILE_SIZE -eq 0 ]; then
         echo Removed $f since empty
         rm $f
-    elif [ "$(wc -l $f)" -le 10 ]; then 
+    elif [ $FILE_SIZE -le 10 -a $FILE_SIZE -gt 0 ]; then 
         echo Less than 10: $f
         mv {,short_}$f
-    elif [ "$(wc -l $f)" -ge 10 -a "$(wc -l $f)" -le 20 ]; then 
+    elif [ $FILE_SIZE -ge 10 -a $FILE_SIZE -le 20 ]; then 
         echo Between 10 and 20: $f
         mv {,medium_}$f
-    elif [ "$(wc -l $f)" -ge 20 ]; then 
+    elif [ $FILE_SIZE -ge 20 ]; then 
         echo Greater than 20: $f
         mv {,long_}$f
     fi
