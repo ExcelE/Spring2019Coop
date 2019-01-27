a4448f013b1bfa14648879981b363ea6530d2ae6
Excel Espina <37338625+ExcelE@users.noreply.github.com>
2019-01-27 01:59:49 -0500

    Update file_rename.sh
    Changed echo output for better visibility

--- coop_asmt1/file_rename.sh
+++ coop_asmt1/file_rename.sh
@@ -8,14 +8,14 @@ do
     if [ $FILE_SIZE -eq 0 ]; then
         echo Removed $f since empty
         rm $f
-    elif [ $FILE_SIZE -le 10 -a $FILE_SIZE -gt 0 ]; then 
-        echo Less than 10: $f
+    elif [ $FILE_SIZE -lt 10 -a $FILE_SIZE -gt 0 ]; then 
+        echo $f: Less than 10 \($FILE_SIZE lines\)
         mv {,short_}$f
     elif [ $FILE_SIZE -ge 10 -a $FILE_SIZE -le 20 ]; then 
-        echo Between 10 and 20: $f
+        echo $f: Between 10 and 20 \($FILE_SIZE lines\)
         mv {,medium_}$f
-    elif [ $FILE_SIZE -ge 20 ]; then 
-        echo Greater than 20: $f
+    elif [ $FILE_SIZE -gt 20 ]; then 
+        echo $f: Greater than 20 \($FILE_SIZE lines\)
         mv {,long_}$f
     fi
 done
